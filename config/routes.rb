cf4c307329b00285c43c4fa64335e0d59b4c8f44
Publicover <jameslpublicover@gmail.com>
2016-07-15 11:09:38 -0400

    copy location files from previous version

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'dashboards/index'
 
+  resources :locations
+
   root 'dashboards#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
